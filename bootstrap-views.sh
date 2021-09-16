e0a24c4ec065151ac6c72c063d918e2a813bdf5d
Mohammed Algadhib <mohammed.algadhib@onsemi.com>
2021-09-16 15:30:45 -0700

    added copyright header to all source code files.

--- bootstrap-views.sh
+++ bootstrap-views.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env sh
+##
+## Copyright (c) 2018-2021 onsemi.
+##
+## All rights reserved. This software and/or documentation is licensed by onsemi under
+## limited terms and conditions. The terms and conditions pertaining to the software and/or
+## documentation are available at http://www.onsemi.com/site/pdf/ONSEMI_T&C.pdf (“onsemi Standard
+## Terms and Conditions of Sale, Section 8 Software”).
+##
+
 
 # exit on first error
 set -e
